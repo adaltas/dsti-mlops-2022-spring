2344cbbc8fdbf593dc94e05353030dec98ca637c
pkaferle <62647072+pkaferle@users.noreply.github.com>
2022-03-02 13:03:19 +0100

    Update sample_code.py

--- 04.continuous-testing/sample_code.py
+++ 04.continuous-testing/sample_code.py
@@ -14,16 +14,19 @@ def get_email_from_input():
         return email
 
 # Tests (copy to tests/test_user_functions.py)
+import pytest
+import io
+from user_functions import *
 
-def test_email_with_user_input_no_at_sign(self, monkeypatch):
+def test_email_with_user_input_no_at_sign(monkeypatch):
     monkeypatch.setattr('sys.stdin', io.StringIO('petra.adaltas.com'))
     assert get_email_from_input() is None
 
-def test_email_with_user_input_no_dot(self, monkeypatch):
+def test_email_with_user_input_no_dot(monkeypatch):
     monkeypatch.setattr('sys.stdin', io.StringIO('petra@adaltascom'))
     assert get_email_from_input() is None
 
-def test_email_with_user_input_correct(self, monkeypatch):
+def test_email_with_user_input_correct(monkeypatch):
     monkeypatch.setattr('sys.stdin', io.StringIO('petra@adaltas.com'))
     assert get_email_from_input() == 'petra@adaltas.com'
 
