367f12f8ea9888e436b0408fba2726b43fbd7d4b
pkaferle <62647072+pkaferle@users.noreply.github.com>
2022-03-21 15:42:10 +0100

    module 6: add exercise

---
+++ 06.docker-containers/assets/exercise/04_data_science_essentials/model.py
@@ -0,0 +1,28 @@
+import sklearn
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.svm import LinearSVC
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+
+def prediction():
+    # Exercise A: Load the data
+    
+
+    # Exercise B: Feature Extraction
+
+
+    # Exercise C: Split into Training and Testing Set
+
+
+    # Exercise D: Train the model
+
+
+    # Exercise E: Make Predictions
+
+
+    # change the empty data frame with your predictions data frame
+    results = pd.DataFrame({'foo':[]})
+    return results.to_dict(orient='records')
